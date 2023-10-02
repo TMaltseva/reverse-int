e885edfba5e386d574cd622f97d534aa1eae5a7f
TMaltseva <bacardeonie@gmail.com>
2023-10-02 21:35:48 +0200

    create js (Mon, Oct, 2 2023 9:35:24 PM)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
-}
+    let result = String(Math.abs(n)).split('').reverse().join('');
+    if (result[0] === '0') {
+        result = result.substring(1);
+    };
+    result = parseInt(result);
+    return String(result);       
+};
+
+// console.log(reverse(6200));
+
+
+
+
+
+
