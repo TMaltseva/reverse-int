742cb48b83bde503807239760e0c8cd466448c28
Tommy Maltseva <134083156+TMaltseva@users.noreply.github.com>
2024-08-22 23:13:48 +0300

    fix: return result without intermediate assignment to the result and remove the check for leading zero

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 module.exports = function reverse (n) {
-    let result = String(Math.abs(n)).split('').reverse().join('');
-    if (result[0] === '0') {
-        result = result.substring(1);
-    };
-    result = parseInt(result);
-    return String(result);       
+    return String(parseInt(String(Math.abs(n)).split('').reverse().join('')));
 };
 
 // console.log(reverse(6200));
